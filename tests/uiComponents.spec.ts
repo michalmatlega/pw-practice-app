5b5eb8ca819173b202591d6e06bf24c18d82a2d8
Michal Matlega <m1sz3lek@gmail.com>
2026-01-14 17:09:37 +0100

    l44

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -188,7 +188,7 @@ test('datepicker any day in future', async({page}) => {
   const targetMonthNameShort = targetDate.toLocaleString('default', {month: 'short'});
   const targetDateToAssert = `${targetMonthNameShort} ${targetDay}, ${targetYear}`;
 
-  let currentMonthYearInSelector = await page.locator('nb-calendar-view-mode').textContent();
+  let currentMonthYearInSelector = await page.locator('nb-calendar-view-mode').textContent(); //dont put DOT for non classes!!!
 
   expect(currentMonthYearInSelector).toEqual(' January 2026 ');
 
@@ -200,3 +200,28 @@ test('datepicker any day in future', async({page}) => {
   await page.locator('[class="day-cell ng-star-inserted"]').getByText(targetDay, {exact: true}).click();
   await expect(calendarInputField).toHaveValue(targetDateToAssert);
 });
+
+test('sliders', async({page}) => {
+  //update attribute
+
+  // const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle');
+  // await tempGauge.evaluate(node => {
+  //   node.setAttribute('cx', '232.630');
+  //   node.setAttribute('cy', '232.630');
+  // });
+  // await tempGauge.click();
+
+  //Mouse movement
+  const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger');
+  await tempBox.scrollIntoViewIfNeeded();
+
+  const box = await tempBox.boundingBox();
+  const x = box.x + box.width / 2;
+  const y = box.y + box.height / 2;
+  await page.mouse.move(x, y);
+  await page.mouse.down();
+  await page.mouse.move(x + 100, y);
+  await page.mouse.move(x + 100, y + 100);
+  await page.mouse.up();
+  await expect(tempBox).toContainText('30');
+})
